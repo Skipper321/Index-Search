91db81ce370cbb8d314f233fa6606118c9917514
Max Shih <68304667+Skipper321@users.noreply.github.com>
2025-11-12 15:20:16 -0800

    fixed to only include developer path

--- unzip.py
+++ unzip.py
@@ -4,22 +4,20 @@ import sys
 import zipfile
 
 path = ""
-analyst_path = "analyst.zip"
 developer_path = "developer.zip"
 
 def unzipper(path):
-    if path == "":
-        path = analyst_path
-
     with zipfile.ZipFile(path, 'r') as zip_ana:
         zip_ana.extractall("raw")
-
-unzipper(developer_path)
+        print(f"Extracted '{path}' into 'raw/'")
 
 if __name__ == "__main__":
-    input_path = sys.argv[0]
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    else:
+        path = "developer.zip"  # default fallback
 
-    if (os.path.exists(path)):
+    if os.path.exists(path):
         unzipper(path)
     else:
-        print("path does not exist: ", path)
+        print("path does not exist:", path)
