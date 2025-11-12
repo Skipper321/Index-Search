6e242c22be091091089f35aa2c440d875fd53d24
LoLa Kim <lolak@uci.edu>
2025-11-11 17:47:49 -0800

    Create the basis of the inverted index

--- indexer.py
+++ indexer.py
@@ -1,12 +1,13 @@
-from tokenizer import tokenize
+from tokenizer import tokenize, computeWordFrequencies
 from unzip import unzipper
+from file_items import FileItem
 import os
 import json
 
 def read_zip_contents():
     directory_dict = {}
 
-    unzipper('path_name') # may need to replace path name
+    unzipper('/Users/alohamylola/Desktop/analyst.zip') # may need to replace path name
     base_folder = "raw"
 
     for root, dirs, files in os.walk(base_folder):
@@ -32,5 +33,31 @@ def inverted_index():
     directory_dict = read_zip_contents()
     #  print(directory_dict)
 
+    for site, file_paths in directory_dict.items():
+        for file_path in file_paths:
+            file_item = FileItem(file_path)
+            num_indexed += 1
+            # map document URL to an integer
+            doc_id = doc_id_key
+            doc_ids[doc_id] = file_item.url
+            doc_id_key += 1
+            # load JSON content to tokenize
+            text = file_item.parse_contents()
+            tokens = tokenize(text)
+    
+            token_freq_dict = computeWordFrequencies(tokens)
+            for token, freq in token_freq_dict.items():
+                # check for unique tokens
+                # increase unique_tokens counter and add token to index
+                if token not in index:
+                    index[token] = [] # use lists instead of sets to maintain order
+                    unique_tokens += 1
+                index[token].append((doc_id, freq))
+    return index
+
 if __name__ == "__main__":
-    inverted_index()
+    index = inverted_index()
+    
+    with open("inverted_index.txt", "w", encoding="utf-8") as f:
+        for token, postings in index.items():
+            f.write(f"{token}: {postings}\n")
