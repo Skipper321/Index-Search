8fb0c7b27af006c2f55b4ffc9945eb5797acef47
Max Shih <68304667+Skipper321@users.noreply.github.com>
2025-11-18 11:08:36 -0800

    added necessary requirements for M2, need prompt

--- search.py
+++ search.py
@@ -1,114 +1,126 @@
-import json
+import json, csv, math, struct, heapq
 import os.path
-import indexer
 import tokenizer
+from collections import defaultdict
+# cannot import this! let the indexer write the files then search reads these files only
+# import indexer
+
 
 class SearchEngine:
 
-    def __init__(self, final_index_path="final_index.json"):
-        self.path = final_index_path
+    def __init__(self):
+        # Load dictionary into memory (small)
+        with open("index/dictionary.csv", "r", encoding="utf-8") as f:
+            rdr = csv.DictReader(f)
+            self.dictionary = {
+                row["term"]: (int(row["df"]), int(row["offset"]), int(row["length"]))
+                for row in rdr
+            }
+        
+        with open("doc_ids.json", "r", encoding="utf-8") as f:
+            self.doc_ids = json.load(f) # keys are strings
 
-    # Main function which generates search results
-    #
-    # Takes a search query
-    # Returns dictionary results (for now)
-    def searchFor(self, input):
-        # TODO: need editing
-        return
+        with open("index/corpus_meta.json", "r", encoding="utf-8") as f:
+            meta = json.load(f)
+        self.N = int(meta["N"])
+
+        self.postings_path = "index/postings.bin"
+
+        print(f"[INFO] Loaded dictionary with {len(self.dictionary)} terms.")
+        print(f"[INFO] Ready to search {self.N} documents.")
+
+
+    def read_postings(self, term):
+        info = self.dictionary.get(term)
+        if not info:
+            return []
+        
+        df, offset, length = info
+
+        postings = []
+        with open(self.postings_path, "rb") as f:
+            f.seek(offset)
+            block = f.read(length)
+
+        # Decode each (doc_id:int32, tf:float32)
+        for i in range(0, length, 8):
+            doc_id, tf = struct.unpack_from("<if", block, i)
+            postings.append((doc_id, tf))
+
+        return postings
+    
+    # TF-IDF weighting
+    def idf(self, df):
+        return math.log((self.N + 1) / (df + 0.5)) + 1.0
+
 
     # Searches for one term at a time
-    #
     # Takes tokenized search query
     # Returns positional indices for each search as a dictionary
-    def searchOne(self, term, index_path=""):
-        if index_path == "":
-            index_path = self.path
-
-        docid_and_weight = {}
-
-        with open(index_path) as f:
-            data = json.load(f)
-            # result = [data[x] for x in data if x == term]
-
-            if (data[term]):
-                for result in data[term]:
-                    docid = result[0]
-                    weight = result[1]
-
-                    # TODO: we want to save the position as well right? not just docid and weight
-                    # so that it still works if we want a search query with two words next to each other
-                    # and not just one which allows us to know that it's present in the document
-
-                    docid_and_weight[docid] = weight
+    def searchOne(self, term):
+        term = tokenizer.tokenize(term)
+        if len(term) == 0:
+            return {}
         
-        print("Searched the term ", term , " and found the following doc_ids and weights: ", docid_and_weight)
+        t = term[0] # tokenize returns a list 
+        if t not in self.dictionary:
+            print(f"[INFO] '{t}' not found.")
+            return {}
         
-        return docid_and_weight
+        df, offset, length = self.dictionary[t]
+        postings = self.read_postings(t)
 
-    # Searches for the phrase positionally
-    # 
-    # Takes positional indices from a dictionary
-    # Returns a common dictionary
-    def searchAllTerms(self, query, index_path=""):
-        query_tokens = tokenizer.tokenize(query)
-        common = {}
+        docid_weight = {doc_id: tf for doc_id, tf in postings}
+        print(f"[INFO] searchOne('{term}') yielded >>> {len(docid_weight)} results")
+        return docid_weight
 
-        # `common` - Represents all locations of search terms that show up in the same place
-        # key = location
+    # Searches for multiple terms with TF-IDF scoring
+    def searchFor(self, query, top_k=10):
+        q_terms = tokenizer.tokenize(query)
+        if not q_terms:
+            return []
+        
+        scores = defaultdict(float)
 
-        # value = list of weights
-        # position of list = weight of query term
+        for t in q_terms:
+            if t not in self.dictionary:
+                continue
 
-        # for example, query = "john steinbeck" 
-        # "john" -> [3, 1.3]
-        # "steinbeck" -> [3, 4.2] [6, 7.2]
-        # common = {3, [1.3, 4.2]}
-        # query[0] = "john", common[3][0] = 1.3 (weight of "john")
+            df, _, _ = self.dictionary[t]
+            idf_weight = self.idf(df)
 
-                    
-        return common 
+            postings = self.read_postings(t)
+            for doc_id, tf in postings:
+                tfw = 1 + math.log(max(tf,1e-6))
+                scores[doc_id] += tfw * idf_weight
 
-    def printCommon(self, query, index_path):
-        print("Searched for ", query, " within file ", index_path)
-        print(self.searchAllTerms(query, index_path))
-    
+        if not scores:
+            return []
+        
+        # top-k 
+        top = heapq.nlargest(top_k, scores.items(), key=lambda x: x[1])
 
-    # Takes two dictionaries and merges it
-    # Keys = docid
-    # Values = weights
-    # Position for values = position of word in query
-    # Values are in a list
-    def getCommon(dict1, dict2):
-        merged = {}
+        # convert doc_id >>> url 
+        results = [(self.doc_ids[str(doc)], score) for doc, score in top]
+        return results
 
-        d1 = set(dict1)
-        d2 = set(dict2)
+    # We don't need full phrase search or commons
+    # def searchAllterms(self, query): 
 
-        common_keys = d1.intersection(d2)
-
-        for key in common_keys:
-            merged[key] = [dict1[key], dict2[key]]
-
-        return merged
-    
-    def results(self):
-        return self.results
+    def printResults(self, results):
+        if not results:
+            return 
+        
+        for i, (url, score) in enumerate(results, start=1):
+            print(f"{i}.{url} (score={score:.4f})")
 
 
 if __name__ == "__main__":
-    input = [
-        "cristina lopes", 
-        "machine learning",
-        "ACM",
-        "master of software engineering"]
-
-    # NOTE: sorry I haven't used this yet because i literally didn't get the results yet so i'm only really able to search within index_part_0.json
-    final_index = "final_index.json" 
-    default_path = "index/index_part_0.json"
+    # Testings sample queries 
+    engine = SearchEngine()
+    print("--- Sample Queries ---")
+    for q in ["machine learning", "cristina lopes", "ACM", "undergraduate research"]:
+        print("\nQuery:", q)
+        res = engine.searchFor(q)
+        engine.printResults(res)
     
-    # TODO: need to change this for the actual engine
-    # this is just a test
-
-    sg = SearchEngine(default_path)
-    sg.searchOne("artificial")
-    sg.printCommon("machine learning", final_index)
