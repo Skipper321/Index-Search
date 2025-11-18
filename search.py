b19927e49f02d88ff98f3457df8d1390d3ebdbda
Julie Bui <minhab@uci.edu>
2025-11-17 19:15:29 -0800

    updated search to tokenize query

--- search.py
+++ search.py
@@ -1,6 +1,14 @@
+import json
 import os.path
 import indexer
+import tokenizer
 
+# Searches for one term
+def searchOne(term, index_path):
+    with open(index_path) as f:
+        data = json.load(f)
+        result = [data[x] for x in data if x == term]
+        print(term, ": ", result)
 
 
 if __name__ == "__main__":
@@ -10,4 +18,12 @@ if __name__ == "__main__":
         "ACM",
         "master of software engineering"]
     
-    index_path = "index_final.json"
+    default_path = "index_final.json"
+    
+    # TODO: need to change this for the actual engine
+    # this is just a test
+    for queries in example_queries:
+        query_tokens = tokenizer.tokenize(queries)
+
+        for token in query_tokens:
+            searchOne(token, "index_part_0.json")
