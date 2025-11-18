b77aa2799a890542aaefcfdaf43cceffe8fedee3
Julie Bui <minhab@uci.edu>
2025-11-17 18:55:31 -0800

    added search file

---
+++ search.py
@@ -0,0 +1,13 @@
+import os.path
+import indexer
+
+
+
+if __name__ == "__main__":
+    example_queries = [
+        "cristina lopes", 
+        "machine learning",
+        "ACM",
+        "master of software engineering"]
+    
+    index_path = "index_final.json"
